9a98c6b0029556f6634ff3ab6e377946cc5a5fa3
suda27 <susasan27@gmail.com>
2025-07-06 21:28:57 +0530

    Add Section navigation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -28,4 +28,23 @@ export class NavbarComponent {
     // Then navigate to home if not already there
     this.router.navigate(['/'], { replaceUrl: true });
   }
+
+  scrollToContact() {
+    // If not on home, navigate to home first, then scroll
+    if (this.router.url !== '/') {
+      this.router.navigate(['/#contact']).then(() => {
+        setTimeout(() => {
+          const el = document.getElementById('contact');
+          if (el) {
+            el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+          }
+        }, 200);
+      });
+    } else {
+      const el = document.getElementById('contact');
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  }
 }
